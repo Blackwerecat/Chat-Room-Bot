7db96f169d564d49e115752aed780096d94e047d
Blackwerecat <31685571+Blackwerecat@users.noreply.github.com>
2017-11-21 13:10:36 -0800

    Create event-caller.js

---
+++ event-caller.js
@@ -0,0 +1,18 @@
+var EVENTS = {events:{},
+	bind:function(event,func) {
+		if (this.events[event]) {
+			this.events[event].push(func);
+				return events.length-1
+			}else {
+				this.events[event] = [func];
+			return 0;
+		}
+	},
+	unbind:function(event,index) {
+		this.events[event].splice(index,index+1);
+	},
+	call:function(event,payload) {
+		for (i in this.events[event]) {
+			this.events[event][i](payload);
+		}
+	}};
