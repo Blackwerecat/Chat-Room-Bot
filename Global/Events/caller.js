7cb4789f34ef4d58d5fda66ebc15a13cdf14b087
Blackwerecat <31685571+Blackwerecat@users.noreply.github.com>
2017-11-27 15:50:05 -0800

    Delete caller.js

--- Global/Events/caller.js
+++
@@ -1,6 +0,0 @@
-//Set up the events here, such as the message event
-var messageEvent = Classroom.events.publicMessageEvent;
-Classroom.events.publicMessageEvent = function(payload) {
-	EVENTS.call('message-event',payload);
-	messageEvent(payload);
-};
